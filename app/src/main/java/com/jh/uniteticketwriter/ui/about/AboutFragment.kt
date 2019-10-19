3dae8e77cf8ee022303d5083c178b0ce3e53e8f5
WandererN <izhdanovn@gmail.com>
2019-10-20 00:15:16 +0300

    added AboutFragment

---
+++ app/src/main/java/com/jh/uniteticketwriter/ui/about/AboutFragment.kt
@@ -0,0 +1,23 @@
+package com.jh.uniteticketwriter.ui.about
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.jh.uniteticketwriter.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.activity_about, container, false)
+
+}
+
