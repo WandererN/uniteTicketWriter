a34191ea7299f1689c878c5e6661b782f0bb4794
WandererN <izhdanovn@gmail.com>
2019-10-19 23:42:44 +0300

    AppLaunch action added

---
+++ app/src/main/java/com/jh/uniteticketwriter/nfc/message/applaunch/AppLauchNfcMessage.kt
@@ -0,0 +1,30 @@
+package com.jh.uniteticketwriter.nfc.message.applaunch
+
+import com.jh.uniteticketwriter.extensions.readString
+import com.jh.uniteticketwriter.extensions.writeString
+import com.jh.uniteticketwriter.nfc.message.CustomNfcMessage
+import com.jh.uniteticketwriter.nfc.message.MessageNfcTypes
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+class AppLauchNfcMessage(override var message: AppLaunchRecord? = null) :
+    CustomNfcMessage<AppLaunchRecord> {
+
+    override val type = MessageNfcTypes.APP_LAUNCH.toInt()
+
+    override fun toByteArray(): ByteArray = ByteArrayOutputStream().apply {
+        use {
+            message?.let { mess ->
+                it.writeString(mess.appName)
+                it.writeString(mess.appPackage)
+            }
+        }
+    }.toByteArray()
+
+    override fun parse(data: ByteArray) = apply {
+        ByteArrayInputStream(data).use {
+            message = AppLaunchRecord(it.readString(), it.readString())
+        }
+    }
+
+}
---
+++ app/src/main/java/com/jh/uniteticketwriter/nfc/message/applaunch/AppLaunchRecord.kt
@@ -0,0 +1,3 @@
+package com.jh.uniteticketwriter.nfc.message.applaunch
+
+class AppLaunchRecord(val appName: String = "", val appPackage: String = "")
