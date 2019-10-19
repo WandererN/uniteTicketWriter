5b3bdf1edbe8ed95e5c9c4e08ae76753deb03c3d
WandererN <izhdanovn@gmail.com>
2019-10-19 18:00:25 +0300

    added nav_graph and MainFragmentActivity

---
+++ app/src/main/java/com/jh/uniteticketwriter/ui/MainFragmentActivity.kt
@@ -0,0 +1,15 @@
+package com.jh.uniteticketwriter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jh.uniteticketwriter.R
+import com.jh.uniteticketwriter.ui.main.MainFragment
+
+class MainFragmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
