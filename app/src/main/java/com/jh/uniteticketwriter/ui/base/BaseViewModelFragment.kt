453b8d254c26209197d453012db82a3c50e6f091
WandererN <izhdanovn@gmail.com>
2019-10-20 14:49:59 +0300

    implement BaseViewModelFragment

---
+++ app/src/main/java/com/jh/uniteticketwriter/ui/base/BaseViewModelFragment.kt
@@ -0,0 +1,31 @@
+package com.jh.uniteticketwriter.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+
+
+abstract class BaseViewModelFragment<T : ViewModel>(
+    val layoutId: Int,
+    val viewModelClass: Class<T>
+) :
+    Fragment() {
+    protected lateinit var viewModel: T
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutId, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(viewModelClass)
+        // TODO: Use the ViewModel
+    }
+
+}
