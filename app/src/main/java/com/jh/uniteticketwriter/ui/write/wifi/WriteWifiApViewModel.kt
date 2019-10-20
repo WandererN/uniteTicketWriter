95d2148e6936a7dfd8580d183cb7ea38e75d3bc3
WandererN <izhdanovn@gmail.com>
2019-10-20 23:43:10 +0300

    make WriteWifiApViewModel implement WriteViewModelInterface; supply write_wifi_ap_fragment with lifedata

--- app/src/main/java/com/jh/uniteticketwriter/ui/write/wifi/WriteWifiApViewModel.kt
+++ app/src/main/java/com/jh/uniteticketwriter/ui/write/wifi/WriteWifiApViewModel.kt
@@ -1,7 +1,38 @@
 package com.jh.uniteticketwriter.ui.write.wifi
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jh.uniteticketwriter.nfc.message.CustomNfcMessage
+import com.jh.uniteticketwriter.nfc.message.wifi.AuthTypes
+import com.jh.uniteticketwriter.nfc.message.wifi.WifiNfcMessage
+import com.jh.uniteticketwriter.nfc.message.wifi.WifiRecord
+import com.jh.uniteticketwriter.ui.write.WriteViewModel
 
-class WriteWifiApViewModel : ViewModel() {
+class WriteWifiApViewModel : ViewModel(), WriteViewModel {
+    override fun validate(): Boolean {
+        return !ssid.value.isNullOrEmpty()
+    }
+
+    override fun getMessage(): CustomNfcMessage<*> {
+        return WifiNfcMessage(
+            WifiRecord(
+                AuthTypes.fromInt(authType.value ?: 0)
+                    ?: AuthTypes.NO_AUTH,
+                ssid.value ?: "",
+                password.value ?: "",
+                userName.value ?: ""
+            )
+        )
+    }
+
     // TODO: Implement the ViewModel
+    var authType = MutableLiveData<Int>() //TODO make spinner data binding
+    val ssid = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val userName = MutableLiveData<String>()
+    val userNameVisibility = MutableLiveData<Int>()
+
+    init {
+        userNameVisibility.value = 1
+    }
 }
